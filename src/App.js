4227b6775c53c6598f1207d123e6381e4d8148d7
SIVA BALAN <sivabalan.cs23@bitsathy.ac.in>
2024-04-24 07:07:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import your CSS file for styling
 
-function App() {
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+
+  const handleInputChange = (event) => {
+    setTaskInput(event.target.value);
+  };
+
+  const addItem = () => {
+    if (taskInput.trim() !== '') {
+      setTasks([...tasks, taskInput]);
+      setTaskInput('');
+    }
+  };
+
+  const deleteItem = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>TO DO LIST</h1>
+      <div className="input">
+        <input className="inpt" type="text" value={taskInput} onChange={handleInputChange} placeholder="Enter task"/>
+        <button className="inp" onClick={addItem}>ADD</button>
+      </div>
+      <div className="taskList">
+        {tasks.map((task, index) => (
+          <div key={index} className="list-item">
+            <div>{task}</div>
+            <button className="delete-btn" onClick={() => deleteItem(index)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
